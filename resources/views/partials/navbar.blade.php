0f2d4b6dff299a44809e9fd79997c909b7cee17f
Javi <1832517@alu.murciaeduca.es>
2019-02-06 13:04:22 +0100

    Esqueleto y primeras vistas

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="/" style="color:#777"><span style="font-size:15pt">&#9820;</span> Lista de la Compra</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        @if( true || Auth::check() )
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item {{ Request::is('productos') && ! Request::is('productos/create')? 'active' : ''}}">
+                        <a class="nav-link" href="{{url('/productos')}}">
+                            <span class="glyphicon glyphicon-film" aria-hidden="true"></span>
+                            Productos
+                        </a>
+                    </li>
+                    <li class="nav-item {{  Request::is('productos/create') ? 'active' : ''}}">
+                        <a class="nav-link" href="{{url('/productos/create')}}">
+                            <span>&#10010</span> Nuevo Producto
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav navbar-right">
+                    <li class="nav-item">
+                        <form action="{{ url('/logout') }}" method="POST" style="display:inline">
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-link nav-link" style="display:inline;cursor:pointer">
+                                Cerrar sesión
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        @endif
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/logout', function () {
+    return view('logout');
+});
+Route::get('/productos', function () {
+    return view('productos.index');
+});
+Route::get('/productos/show/{id}', function ($id) {
+    return view('productos.show',array('id'=>$id));
+});
+Route::get('/productos/create', function () {
+    return view('productos.create');
+});
+Route::get('/productos/edit/{id}', function ($id) {
+    return view('productos.edit',array('id'=>$id));
 });
